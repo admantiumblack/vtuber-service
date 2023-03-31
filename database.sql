c14e21f23755b887bef525cc07305709d204e935
Bryan Aleron <83805316+Neltharion193@users.noreply.github.com>
2023-04-01 00:36:50 +0700

    push database

--- database.sql
+++ database.sql
@@ -112,7 +112,7 @@ INSERT INTO `vtuber_platform` (`channel_id`, `vtuber_id`, `platform_id`, `channe
 ('UC1uv2Oq6kNxgATlCiez59hw',	4,	1,	'Towa Ch. 常闇トワ',	'2001-01-01 00:00:00',	'9999-12-31 00:00:00'),
 ('UCahgMxSIQ2zIRrPKhM6Mjvg',	2,	1,	'Mika Melatika【NIJISANJI・にじさんじ】',	'2001-01-01 00:00:00',	'9999-12-31 00:00:00'),
 ('UChgTyjG-pdNvxxhdsXfHQ5Q',	6,	1,	'Pavolia Reine Ch. hololive-ID',	'2001-01-01 00:00:00',	'9999-12-31 00:00:00'),
-('UCJFZiqLMntJufDCHc6bQixg',	3,	1,	'Suisei Channel',	'2012-01-01 00:00:00',	'9999-12-31 00:00:00'),
+('UC5CwaMl1eIgY8h02uZw7u8A',	3,	1,	'Suisei Channel',	'2012-01-01 00:00:00',	'9999-12-31 00:00:00'),
 ('UCL_qhgtOy0dy1Agp8vkySQg',	5,	1,	'Mori Calliope Ch. hololive-EN',	'2023-03-31 15:22:20',	'9999-12-31 00:00:00'),
 ('UCLHmLrj4pHHg3-iBJn_CqxA',	9,	1,	'Neuro-sama',	'2001-01-01 00:00:00',	'9999-12-31 00:00:00'),
 ('UCP0BspO_AMEe3aQqqpo89Dg',	1,	1,	'Moona Hoshinova hololive-ID',	'2023-03-31 15:45:54',	'9999-12-31 00:00:00'),
